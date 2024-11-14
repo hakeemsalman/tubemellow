175b7e0065d1c3bb45ab6f74421d47c980816a01
hakeemsalman <salman74163@gmail.com>
2024-11-14 17:42:00 +0530

    add icons, copy-webpack-plugin and updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+import CopyPlugin from "copy-webpack-plugin"
+
 module.exports = {
   mode: "development",  
   entry: "/src/test.tsx",
@@ -7,9 +9,20 @@ module.exports = {
         use: "ts-loader",
         test: /\.tsx$/,
         exclude: /node_modules/,
-      }
+      },
     ]
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: Path2D.resolve('src/manifest.json'), to: path.resolve('dist/') },
+        { from: Path2D.resolve('src/assets/icon-16.png'), to: path.resolve('dist/assets/icon-16.png') },
+        { from: Path2D.resolve('src/assets/icon-32.png'), to: path.resolve('dist/assets/icon-32.png') },
+        { from: Path2D.resolve('src/assets/icon-48.png'), to: path.resolve('dist/assets/icon-48.png') },
+        { from: Path2D.resolve('src/assets/icon-128.png'), to: path.resolve('dist/assets/icon-128.png') },
+      ],
+    }),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
