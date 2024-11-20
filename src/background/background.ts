6470b7dded2528479817ffadb045bcceb4148d38
hakeemsalman <salman74163@gmail.com>
2024-11-20 16:10:04 +0530

    migrating to vite from webpack

--- src/background/background.ts
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('You have run the chrome extension')
-})
-
-chrome.bookmarks.onCreated.addListener(() => {
-  console.log('You have just bookmarked the page.')
-})
