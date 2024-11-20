20bd521fa52b54211b575089fbc3fcd2d47e2e39
hakeemsalman <salman74163@gmail.com>
2024-11-20 16:39:25 +0530

    add vite configuration and tailwind css and clean some UI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: [
+            'src/static/icon-16.png',
+            'src/static/icon-32.png',
+            'src/static/icon-48.png',
+            'src/static/icon-128.png'
+          ],
+          dest: 'assets/'
+        },
+        {
+          src: 'src/static/manifest.json',
+          dest: '.'
+        },
+        {
+          src: ['src/content_script.js', 'src/background.js'],
+          dest: 'scripts/'
+        }
+      ]
+    })
+  ],
 })
