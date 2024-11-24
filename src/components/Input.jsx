972389535f28be08f99086b4e22cf653970f8d68
hakeemsalman <salman74163@gmail.com>
2024-11-24 12:08:46 +0530

    add name with data

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Input({ onChange, isToggle, id}) {
+export default function Input({ onChange, isToggle, id, data}) {
 
   const handleToggle = (e) => {
     onChange(e);
@@ -10,7 +10,7 @@ export default function Input({ onChange, isToggle, id}) {
 
   return (
     <div className="relative inline-block w-11 h-5" >
-      <input checked={isChecked ? true : false} id={id} onChange={handleToggle} type="checkbox" className="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-slate-800 cursor-pointer transition-colors duration-300" />
+      <input checked={isChecked ? true : false} id={id} onChange={handleToggle} name={data} type="checkbox" className="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-slate-800 cursor-pointer transition-colors duration-300" />
       <label htmlFor={id} className="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-slate-800 cursor-pointer"></label>
     </div>
   )
