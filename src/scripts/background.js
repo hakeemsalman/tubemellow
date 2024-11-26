4b5364b72cedb19955a2df0fd24553fff36c763b
hakeemsalman <salman74163@gmail.com>
2024-11-26 11:47:51 +0530

    add badge text

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -3,23 +3,37 @@ chrome.tabs.onActivated.addListener(() => {
 });
 
 chrome.tabs.onUpdated.addListener(() => {
-  checkActiveTab();
+    checkActiveTab();
 });
+
 
 
 function checkActiveTab() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length === 0) return;
 
-    const activeTab = tabs[0];
-    const url = activeTab.url;
+    try {
+      console.log("Active tab details:", tabs[0]); // Debug the active tab's properties
+      const activeTab = tabs[0];
+      const url = activeTab.url || "";
 
-    if (url && url.includes("youtube.com")) {
-      chrome.action.setIcon({ path: chrome.runtime.getURL("assets/icon-16.png") });
-      chrome.action.enable();
-    } else {
-      chrome.action.setIcon({ path: chrome.runtime.getURL("assets/icon-16-disabled.png") });
-      chrome.action.disable();
+      if (url && url.includes("youtube.com")) {
+        chrome.action.setIcon({ path: chrome.runtime.getURL("assets/icon-16.png") });
+        chrome.action.enable();
+         chrome.action.setBadgeText({
+          tabId: activeTab.id,
+          text: 'ON',
+        });
+      } else {
+        chrome.action.setIcon({ path: chrome.runtime.getURL("assets/icon-16-disabled.png") });
+        chrome.action.disable();
+        chrome.action.setBadgeText({
+         tabId: activeTab.id,
+         text: 'OFF',
+       });
+      }
+    } catch (error) {
+      console.error("Error checking active tab:", error);
     }
   });
 }
