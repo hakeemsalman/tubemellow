c370c157c1e8e4f1dabf13590e872a22762ae5e4
hakeemsalman <salman74163@gmail.com>
2024-12-05 00:00:45 +0530

    replace manrope to roboto font and add naskh font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "@fontsource/manrope/latin-400.css"; // Defaults to weight 400
+import "@fontsource/roboto/latin-400.css"; // Defaults to weight 400
+
 import { I18nProvider } from './packages/locale/i18nprovider.tsx';
 
 createRoot(document.getElementById('root')!).render(
