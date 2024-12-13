68e72830d4af2d3ea7617c40d4ba4f3ebc3b803d
hakeemsalman <salman74163@gmail.com>
2024-12-13 21:36:23 +0530

    pending bookmark

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HideController from './components/HideController.tsx';
 import Footer from './components/Footer.tsx';
 import Accordian from './components/Accordian.tsx';
 import { useTranslation } from 'react-i18next';
+import Bookmarks from './components/Bookmarks.tsx';
 
 function App() {
 const [t] = useTranslation();
@@ -19,6 +20,9 @@ const [t] = useTranslation();
       <div className='px-3 py-2 flex flex-col h-full justify-start'>
         <Accordian title={t('common.yt_title')} id='youtube_content'>
           <HideController />
+        </Accordian>
+        <Accordian title={t('common.bookmark')} id='bookmark'>
+          <Bookmarks />
         </Accordian>
       </div>
       <div className='justify-end'>
