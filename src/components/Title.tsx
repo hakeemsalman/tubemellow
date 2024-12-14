3c517a0bf8e4dd7c9fa8933d818c8c0f499fe9ee
hakeemsalman <salman74163@gmail.com>
2024-12-14 12:15:17 +0530

    modify color of the title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ export default function Title() {
   return (
     <div className='flex flex-row gap-3 items-center'>
       <img src='/assets/icon-32.png' />
-      <span className='text-xl text-blue-700'>Tube Mellow</span>
+      <span className='text-xl text-secondary font-semibold'>Tube Mellow</span>
     </div>
   )
 }
